dfdd5c142be05de72a0f8a06f58f072c14806df7
Lenovo <@>
2022-12-22 19:53:35 +0300

    advertising

---
+++ vebsite/main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('<h4>Hello Django</h4>')
+
+def advertising(request):
+    return HttpResponse('<h2>your ad can be here</h2>')
