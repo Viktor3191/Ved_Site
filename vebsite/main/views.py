61da893f529e882a3f49df984e9538491324adf5
Lenovo <@>
2022-12-22 21:18:43 +0300

    dz_new page

--- vebsite/main/views.py
+++ vebsite/main/views.py
@@ -4,5 +4,8 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse('<h4>Hello Django</h4>')
 
+def about(request):
+    return HttpResponse('<h4>About</h4>')
+
 def advertising(request):
     return HttpResponse('<h2>your ad can be here</h2>')
