1328cceebfe61a7deb7b0e9653bb6618d26c63b1
Lenovo <@>
2023-01-27 00:25:22 +0300

    dz_создание доп поля в альтернативе. Доработка поля в форме.

--- vebsite/main/forms.py
+++ vebsite/main/forms.py
@@ -1,8 +1,13 @@
+from dataclasses import fields
 from .models import Task  # импортируем из нашего приложения
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets, TextInput
 
 
 class TaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ["title", "task", "decision"]
+        widgets = {'title': TextInput(attrs={'class': 'form-control', 'placeholder': 'введите название'}),
+                   'task': TextInput(attrs={'class': 'form-control', 'placeholder': 'введите описание'}),
+                   'decision': TextInput(attrs={'class': 'form-control', 'placeholder': 'введите решение'})
+                   }
