b7ba46715fe86ce43f82a1b1ce8f80afb00087f2
Lenovo <@>
2023-01-29 17:24:13 +0300

    dz_создание доп поля в альтернативе. Шаблон для полей в форме

--- vebsite/main/forms.py
+++ vebsite/main/forms.py
@@ -1,13 +1,13 @@
 from dataclasses import fields
 from .models import Task  # импортируем из нашего приложения
-from django.forms import ModelForm, widgets, TextInput
+from django.forms import ModelForm, TextInput, Textarea, widgets
 
 
 class TaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ["title", "task", "decision"]
-        widgets = {'title': TextInput(attrs={'class': 'form-control', 'placeholder': 'введите название'}),
-                   'task': TextInput(attrs={'class': 'form-control', 'placeholder': 'введите описание'}),
-                   'decision': TextInput(attrs={'class': 'form-control', 'placeholder': 'введите решение'})
+        widgets = {"title": TextInput(attrs={'class': 'form-control', 'placeholder': 'введите название'}),
+                   "task": Textarea(attrs={'class': 'form-control', 'placeholder': 'введите описание'}),
+                   "decision": Textarea(attrs={'class': 'form-control', 'placeholder': 'введите решение'})
                    }
